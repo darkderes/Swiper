29ee9dcdc56c1c43191ec229d872619665618696
Jorge Darderes <jdarderes@gmail.com>
2021-08-01 20:14:37 -0400

    Add project files.

---
+++ Swiper/Swiper/Controls/SwiperControl.xaml.cs
@@ -0,0 +1,158 @@
+﻿using Swiper.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Swiper.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SwiperControl : ContentView
+    {
+        private readonly double _initialRotation;
+        private static readonly Random _random= new Random();
+        private double _screendWidth = -1;
+
+        private const double DeadZone = 0.4d;
+        private const double DecisionThreshold = 0.4d;
+
+        public event EventHandler Onlike;
+        public event EventHandler OnDeny;
+
+        public SwiperControl()
+        {
+            InitializeComponent();
+
+            var panGesture = new PanGestureRecognizer();
+            panGesture.PanUpdated += OnPanUpdate;
+            this.GestureRecognizers.Add(panGesture);
+
+            _initialRotation = _random.Next(-10,10);
+            photo.RotateTo(_initialRotation,100,Easing.SinOut);
+
+            //other code ommitted for brevity
+
+
+            var picture = new Picture();
+            descriptionLabel.Text = picture.Description;
+            image.Source = new UriImageSource() { Uri = picture.Uri };
+
+            loadingLabel.SetBinding(IsVisibleProperty, "IsLoading");
+            loadingLabel.BindingContext = image;
+        }
+        private void CalculatePanState(double panX)
+        {
+            var halfScreenWidth = _screendWidth / 2;
+            var deadZoneEnd = DeadZone * halfScreenWidth;
+
+            if (Math.Abs(panX) < deadZoneEnd)
+            {
+                return;
+            }
+
+            var passedDeadzone = panX < 0 ? panX + deadZoneEnd : panX - deadZoneEnd;
+            var decisionZoneEnd = DecisionThreshold * halfScreenWidth;
+            var opacity = passedDeadzone / decisionZoneEnd;
+
+            opacity = Clamp(opacity,-1,1);
+
+            likeStackLayout.Opacity = opacity;
+            denyStackLayout.Opacity = -opacity;
+        }
+
+
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+
+            if (Application.Current.MainPage == null)
+            {
+                return;
+            }
+            _screendWidth = Application.Current.MainPage.Width;
+        }
+        private void OnPanUpdate( object sender, PanUpdatedEventArgs e)
+        {
+            switch (e.StatusType)
+            {
+                case GestureStatus.Started:
+                    PanStarted();
+                    break;
+                case GestureStatus.Running:
+                    PanRunning(e);
+                    break;
+                case GestureStatus.Completed:
+                    PanCompleted();
+                    break;
+            }        
+        }
+
+        private void PanCompleted()
+        {
+            if (CheckForExitCriteria())
+            {
+                Exit();
+            }
+
+            likeStackLayout.Opacity = 0;
+            denyStackLayout.Opacity = 0;
+
+            photo.TranslateTo(0, 0 ,250 ,Easing.SpringOut);
+            photo.RotateTo(_initialRotation,250,Easing.SpringOut);
+            photo.ScaleTo(1, 250);
+        }
+
+        private void PanRunning(PanUpdatedEventArgs e)
+        {
+            photo.TranslationX = e.TotalX;
+            photo.TranslationY = e.TotalY;
+            photo.Rotation = _initialRotation + (photo.TranslationX / 25);
+
+            CalculatePanState(e.TotalX);
+        }
+
+        private void PanStarted()
+        {
+            photo.ScaleTo(1.1, 100);
+        }
+
+        private static double Clamp(double value,double min,double max)
+        { 
+           return (value < min) ? min : (value > max )? max : value;
+        }
+        private bool CheckForExitCriteria()
+        { 
+            var halfScreenWidth = _screendWidth / 2;
+            var decisionBreakpoint = DeadZone * halfScreenWidth;
+            return (Math.Abs(photo.TranslationX) > decisionBreakpoint);
+        }
+        private void Exit()
+        {
+            Device.BeginInvokeOnMainThread(async () => {
+
+                var direction = photo.TranslationX < 0 ? -1 : 1;
+
+                if (direction > 0)
+                {
+                    Onlike?.Invoke(this,new EventArgs());
+                }
+
+                if (direction < 0)
+                { 
+                    OnDeny?.Invoke(this,new EventArgs());
+                }
+
+
+                await photo.TranslateTo(photo.TranslationX + (_screendWidth * direction),photo.TranslationY,200,Easing.CubicIn);
+                var parent = Parent as Layout<View>;
+                parent?.Children.Remove(this);        
+            });
+        
+        }
+    }
+}
---
+++ Swiper/Swiper/Utils/DescriptionGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Swiper.Utils
+{
+    public class DescriptionGenerator
+    {
+        private string[] _adjetives = { "Nice","Horrible","great","terrible old","brand new" };
+        private string[] _other = { "picture of grandpa","car","photo of a forest","duck" };
+        
+        private static Random random = new Random();
+
+        public string Generate()
+        {
+            var a = _adjetives[random.Next(_adjetives.Count())];   
+            var b = _other[random.Next(_other.Count())];
+
+            return $"A {a} {b}";
+        }
+    }
+}
